212b4dee704da93012e8ae208cc8d299caf6a5ec
ADIB-the-NOOB <mdadib550@gmail.com>
2023-01-05 23:35:27 +0600

    float number added

--- main.c
+++ main.c
@@ -8,7 +8,20 @@ int main(){
     // printf("%d\n",mynumber);
     // printf("%f\n",myFloatNumber);
     // printf("%c\n",myLetter);
-
     
+    // int x = 5,y = 23, z = 34;
+    // printf("%d",x + y + z);
+
+    // int x, y, z;
+    // x = y = z = 45;
+    // printf("%f",x + y + z);
+    // return 0;
+
+    // float myFloat = 34.55;
+    // printf("%.1f\n",myFloat);
+    // printf("%.2f\n",myFloat);
+    // printf("%.4f\n",myFloat);
+
+
     return 0;
 }
