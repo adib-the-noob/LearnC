7b2a411b4c3d6275c3f6df60a7ba32f71c489124
ADIB-the-NOOB <mdadib550@gmail.com>
2023-01-05 23:08:32 +0600

    first commit

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    // int mynumber = 23;
+    // float myFloatNumber = 222.4;
+    // char myLetter = 'D';
+
+    // printf("%d\n",mynumber);
+    // printf("%f\n",myFloatNumber);
+    // printf("%c\n",myLetter);
+
+    
+    return 0;
+}
