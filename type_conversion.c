0153d9379e141868f98338eedfffe54acc068ae5
ADIB-the-NOOB <mdadib550@gmail.com>
2023-01-06 00:24:47 +0600

    type conversion

---
+++ type_conversion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    // int x = 5;
+    // int y = 6;
+    // int sum = x / y;
+    // printf("%d",sum);
+    float sum = 4;
+    printf("%f",sum);
+    return 0;
+}
